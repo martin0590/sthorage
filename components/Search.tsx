7c59f1289f723f5c3b45adf2325a296d6247108d
Martin M <martin.ale590@gmail.com>
2025-01-31 22:57:36 -0300

    added - main page & fixes

--- components/Search.tsx
+++ components/Search.tsx
@@ -7,50 +7,53 @@ import { getFiles } from "@/lib/actions/file.actions";
 import { Models } from "node-appwrite";
 import Thumbnail from "./Thumbnail";
 import FormattedDateTime from "./FormattedDateTime";
-import { useDebounce } from 'use-debounce'
+import { useDebounce } from "use-debounce";
 
 const Search = () => {
   const [query, setQuery] = useState("");
-  const searchParams = useSearchParams()
-  const searchQuery = searchParams.get('query') || ''
-  const [results, setResults] = useState<Models.Document[]>([])
-  const [open, setOpen] = useState(false)
-  const router = useRouter()
-  const path = usePathname()
-  const [debouncedQuery] = useDebounce(query, 300)
-
-  // TODO: check
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
+  const [debouncedQuery] = useDebounce(query, 300);
 
   useEffect(() => {
-    const fetchFiles = async() => {
-      if(debouncedQuery.length === 0) {
-        setResults([])
-        setOpen(false)
-        return router.push(path.replace(searchParams.toString(), ""))
+    const fetchFiles = async () => {
+      if (debouncedQuery.length === 0) {
+        setResults([]);
+        setOpen(false);
+        return router.push(path.replace(searchParams.toString(), ""));
       }
 
-      const files = await getFiles({types: [], searchText: debouncedQuery})
+      const files = await getFiles({ types: [], searchText: debouncedQuery });
 
-      setResults(files.documents)
-      setOpen(true)
-    }
+      setResults(files.documents);
+      setOpen(true);
+    };
 
-    fetchFiles()
-  },[debouncedQuery])
+    fetchFiles();
+  }, [debouncedQuery]);
 
   useEffect(() => {
-    if(!searchQuery) {
-      setQuery('')
+    if (!searchQuery) {
+      setQuery("");
     }
-  }, [searchQuery])
+  }, [searchQuery]);
 
   const handleClickItem = (file: Models.Document) => {
-    setOpen(false)
-    setResults([])
+    setOpen(false);
+    setResults([]);
 
-    router.push(`/${(file.type === 'video' || file.type === 'audio') ? 'media' : file.type + 's'}?query=${query}`)
-
-  }
+    router.push(
+      `/${
+        file.type === "video" || file.type === "audio"
+          ? "media"
+          : file.type + "s"
+      }?query=${query}`
+    );
+  };
 
   return (
     <div className="search">
@@ -72,16 +75,30 @@ const Search = () => {
         {open && (
           <ul className="search-result">
             {results.length > 0 ? (
-              results.map((file) =>( 
-                <li className="flex items-center justify-between" key={file.$id} onClick={() => handleClickItem(file)}>
+              results.map((file) => (
+                <li
+                  className="flex items-center justify-between"
+                  key={file.$id}
+                  onClick={() => handleClickItem(file)}
+                >
                   <div className="flex cursor-pointer items-center gap-4">
-                    <Thumbnail type={file.type} extension={file.extension} url={file.url} className="size-9 min-w-9" />
-                    <p className="subtitle-2 line-clamp-1 text-light-100">{file.name}</p>
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
                   </div>
 
-                  <FormattedDateTime date={file.$createdAt} className="caption line-clamp-1 text-light-200" />
-                </li>)
-              )
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1 text-light-200"
+                  />
+                </li>
+              ))
             ) : (
               <p className="empty-result">No files found</p>
             )}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,131 +1,142 @@
-'use server'
+"use server";
 
-import { ID, Query } from "node-appwrite"
-import { createAdminClient, createSessionClient } from "../appwrite"
-import { appwriteConfig } from "../appwrite/config"
-import { parseStringify } from "../utils"
-import { cookies } from "next/headers"
-import { redirect } from "next/navigation"
-
+import { ID, Query } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { parseStringify } from "../utils";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const getUserByEmail = async (email: string) => {
-  const { databases } = await createAdminClient()
+  const { databases } = await createAdminClient();
 
   const result = await databases.listDocuments(
     appwriteConfig.databaseId!,
     appwriteConfig.usersCollectionId!,
-    [Query.equal('email', email)]
-  )
+    [Query.equal("email", email)]
+  );
 
-  return result.total > 0 ? result.documents[0] : null
-}
+  return result.total > 0 ? result.documents[0] : null;
+};
 
 const handleError = (error: unknown, message: string) => {
-  console.log(error, message)
-  throw error
-}
+  console.log(error, message);
+  throw error;
+};
 
-export const sendEmailOTP = async({email}: {email: string}) => {
-  const {account} = await createAdminClient()
+export const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
 
   try {
-    const session = await account.createEmailToken(ID.unique(), email)
+    const session = await account.createEmailToken(ID.unique(), email);
 
-    return session.userId
+    return session.userId;
   } catch (error) {
-    handleError(error, 'Failed to send email OTP')
+    handleError(error, "Failed to send email OTP");
   }
-}
+};
 
-export const createAccount = async({ fullName, email}: { fullName: string, email: string }) => {
-  const existingUser = await getUserByEmail(email)
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
 
-  const accountId = await sendEmailOTP({ email})
-  if(!accountId) throw new Error("Failed to send an OTP")
+  const accountId = await sendEmailOTP({ email });
+  if (!accountId) throw new Error("Failed to send an OTP");
 
-    if(!existingUser){
-      const { databases } = await createAdminClient()
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
 
-      await databases.createDocument(
-        appwriteConfig.databaseId!,
-        appwriteConfig.usersCollectionId!,
-        ID.unique(),
-        {
-          fullName,
-          email,
-          avatar: '/images/avatar.png',
-          accountId,
-        }
-      )
+    await databases.createDocument(
+      appwriteConfig.databaseId!,
+      appwriteConfig.usersCollectionId!,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar: "/images/avatar.png",
+        accountId,
+      }
+    );
+  }
 
-    }
+  return parseStringify({ accountId });
+};
 
-    return parseStringify({ accountId })
-}
-
-export const verifySecret = async({accountId, password}: {accountId: string, password: string}) => {
+export const verifySecret = async ({
+  accountId,
+  password,
+}: {
+  accountId: string;
+  password: string;
+}) => {
   try {
-    const { account } = await createAdminClient()
+    const { account } = await createAdminClient();
 
     const session = await account.createSession(accountId, password);
 
-    (await cookies()).set('appwrite-session', session.secret, {
-      path: '/',
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
       httpOnly: true,
-      sameSite: 'strict',
+      sameSite: "strict",
       secure: true,
-    })
+    });
 
-    return parseStringify({ sessionId: session.$id})
-
+    return parseStringify({ sessionId: session.$id });
   } catch (error) {
-    handleError(error, 'Failed to verify OTP')
+    handleError(error, "Failed to verify OTP");
   }
-}
+};
 
 export const getCurrentUser = async () => {
-  const { databases, account } = await createSessionClient()
+  try {
+    const { databases, account } = await createSessionClient();
 
-  const result = await account.get()
+    const result = await account.get();
 
-  const user = await databases.listDocuments(
-    appwriteConfig.databaseId!,
-    appwriteConfig.usersCollectionId!,
-    [Query.equal('accountId', result.$id)]  
-  )
+    const user = await databases.listDocuments(
+      appwriteConfig.databaseId!,
+      appwriteConfig.usersCollectionId!,
+      [Query.equal("accountId", result.$id)]
+    );
 
-  if(user.total <= 0) return null
+    if (user.total <= 0) return null;
 
-  return parseStringify(user.documents[0])
-}
+    return parseStringify(user.documents[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const signOutUser = async () => {
-  const { account } = await createSessionClient()
+  const { account } = await createSessionClient();
 
   try {
-    await account.deleteSession('current');
-    (await cookies()).delete('appwrite-session')
-    
+    await account.deleteSession("current");
+    (await cookies()).delete("appwrite-session");
   } catch (error) {
-    handleError(error, 'Failed to sign out user')
-  }finally {
-    redirect('/sign-in')
+    handleError(error, "Failed to sign out user");
+  } finally {
+    redirect("/sign-in");
   }
-}
+};
 
 export const signInUser = async (email: string) => {
   try {
-    const existingUser = await getUserByEmail(email)
+    const existingUser = await getUserByEmail(email);
 
     //user exists, send OPT
-    if(existingUser){
-      await sendEmailOTP({email})
-      return parseStringify({ accountId: existingUser.accountId })
+    if (existingUser) {
+      await sendEmailOTP({ email });
+      return parseStringify({ accountId: existingUser.accountId });
     }
 
-    parseStringify({ accountId: null, error: 'User not found' })
-
+    parseStringify({ accountId: null, error: "User not found" });
   } catch (error) {
-    handleError(error, "Failed to sign in user")
+    handleError(error, "Failed to sign in user");
   }
-}
+};
