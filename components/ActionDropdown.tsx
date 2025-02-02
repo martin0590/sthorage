b746e635bdb9a6ccc9a570e172c70db89e116ccd
Martin M <martin.ale590@gmail.com>
2025-02-02 13:18:09 -0300

    fixed - imports

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -16,11 +16,9 @@ import { constructDownloadUrl } from "@/lib/utils";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
